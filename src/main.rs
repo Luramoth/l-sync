8bf81ccdad8afd77d9f0505d0d491388b3188c72
LuraMoth <fasreaperearnest@gmail.com>
2022-04-03 17:23:33 -0700

    generate sound when pressing sync button

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::time::Duration;
+
 use fltk::{prelude::*, *};
+use rodio::{OutputStream, Sink, source::SineWave, Source};
 mod ui;
 
 fn main() {
@@ -6,6 +9,14 @@ fn main() {
     let mut ui = ui::UserInterface::make_window();
     ui.but.set_callback(move |_| {
         println!("Sync!");
+
+				let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+				let sink = Sink::try_new(&stream_handle).unwrap();
+
+				let source = SineWave::new(440.0).take_duration(Duration::from_secs_f32(0.25)).amplify(0.9);
+				sink.append(source);
+
+				sink.sleep_until_end();
     });
     app.run().unwrap();
 }
