591c467a5f1bcc5097d6bfef2d1b848f86c89253
LuraMoth <fasreaperearnest@gmail.com>
2022-04-04 00:41:35 -0700

    add in license notices to program files

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,22 @@ use fltk::{prelude::*, *}; // Library for GUI
 use rodio::{OutputStream, Sink, source::SineWave, Source}; // Library for audio playback
 mod ui; // reference to ui.rs
 
+/*  l-sync a content creator syncronisation tool
+ Copyright (C) 2022  Luramoth
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 fn main() {
 	let app = app::App::default();
 	let mut ui = ui::UserInterface::make_window(); // create a window
