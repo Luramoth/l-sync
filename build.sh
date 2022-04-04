b27b6afecd7df54eae6e62c8af27ed5178a1c03a
LuraMoth <fasreaperearnest@gmail.com>
2022-04-04 13:53:23 -0700

    update version for new release

--- build.sh
+++ build.sh
@@ -21,16 +21,23 @@ if [ "$1" = "--windows" ]; then
 	rustup target add x86_64-pc-windows-gnu
 
 	cargo build --target x86_64-pc-windows-gnu $2
+
+	cp l-sync_icon.png target/x86_64-pc-windows-gnu/release
 elif [ "$1" = "--linux" ]; then
 	rustup target add x86_64-unknown-linux-gnu
 
 	cargo build --target x86_64-unknown-linux-gnu $2
+
+	cp l-sync_icon.png target/x86_64-unknown-linux-gnu/release
 elif [ "$1" = "--all" ]; then
 	rustup target add x86_64-pc-windows-gnu
 	rustup target add x86_64-unknown-linux-gnu
 
 	cargo build --target x86_64-pc-windows-gnu $2
 	cargo build --target x86_64-unknown-linux-gnu $2
+
+	cp l-sync_icon.png target/x86_64-unknown-linux-gnu/release
+	cp l-sync_icon.png target/x86_64-pc-windows-gnu/release
 else
 	cargo build $2
 fi
